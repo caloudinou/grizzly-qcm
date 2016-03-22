b999b2a74a069a0daeaf371d56d5f8f04400b05c
caloudinou <pascalou@gmail.com>
2016-03-22 11:47:04 +0100

    fix version : starter project

--- app/components/grizzly-qcm/module.qcm.route.js
+++ app/components/grizzly-qcm/module.qcm.route.js
@@ -2,8 +2,8 @@
     'use strict';
 
     angular.module(angular.mApp.NAMESPACE.qcm.name).config([
-        '$routeProvider','$locationProvider', 
-        function ($routeProvider, $locationProvider) {
+        '$routeProvider',
+        function ($routeProvider) {
             $routeProvider.when(angular.mApp.NAMESPACE.qcm.routes.default.path,{
                 controller  : angular.mApp.NAMESPACE.qcm.controllers.default,
                 templateUrl : angular.mApp.NAMESPACE.qcm.routes.default.templateUrl
